3c4b1f371b6f86e38becc5293e4477a425c411d1
Folkert Stijnman <folkertstijnman@gmail.com>
2021-11-11 21:39:20 +0100

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # close
 App to find alternative activities (small galeries, expositions, live events) in your vicinity
+
+# Gameplan v1
+- Get google maps window
+- Customize window
+- Load venue
+- Info window venue
+
+# Gameplan v2
+T.B.D
---
+++ js/sample_data.js
@@ -0,0 +1,11 @@
+let venue = {
+    "name": "JetBrains Techlab",
+    "description": "Gallery for Humanity and Technology",
+    "addressLine1": "Plantage Middenlaan 21",
+    "latitude": "52.36703480665766",
+    "longitude": "4.910513670122589",
+    "city": "Amsterdam",
+    "postalCode": "1018DB",
+    "email": "info@jetbrainstechlab.com",
+    "phone": "0202402734"
+}
