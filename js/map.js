5125f7eed04d90ed60bf9388f0ee13007d90c3aa
Folkert Stijnman <folkertstijnman@gmail.com>
2021-11-14 17:00:26 +0100

    map & style

---
+++ js/map.js
@@ -0,0 +1,15 @@
+function initMap() {
+    // Create the map.
+
+    var myMapCenter = {
+        lat: 52.3667996,
+        lng: 4.8947421
+    }
+    const map = new google.maps.Map(document.getElementById('map'), {
+        mapId: "a79d604461467bf5",
+        disableDefaultUI: true,
+        zoom: 14,
+        center: myMapCenter,
+    });
+
+}
