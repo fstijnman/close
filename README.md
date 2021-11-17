f074a6d817af159c9af77cd93d8d8eba6a830225
Folkert Stijnman <folkertstijnman@gmail.com>
2021-11-17 20:26:32 +0100

    venue load & front-end setup

--- README.md
+++ README.md
@@ -5,9 +5,16 @@ App to find alternative activities (small galeries, expositions, live events) in
 - Get google maps window DONE
 - Customize window DONE
 - Load venue DONE
-- Venue Marker
-- Info window venue
+- Venue Marker DONE
+
+- Left Side Panel
+
+
 - Opening hours venue
+
+Backlog:
+- Customize markers
+- Markers appearance
 
 # Gameplan v2
 T.B.D
--- js/map.js
+++ js/map.js
@@ -1,7 +1,25 @@
-$.getJSON('http://close-ams.herokuapp.com/venues', function (data) {
-    console.log(data);
-});
-
+let venues = [{
+    "name": "JetBrains Techlab",
+    "description": "Gallery for Humanity and Technology",
+    "addressLine1": "Plantage Middenlaan 21",
+    "latitude": "52.36703480665766",
+    "longitude": "4.910513670122589",
+    "city": "Amsterdam",
+    "postalCode": "1018DB",
+    "email": "info@jetbrainstechlab.com",
+    "phone": "+31202402734"
+},
+{
+    "name": "Museum of the Mind",
+    "description": "Outsider art, in which artists show their imaginative inner world.",
+    "addressLine1": "Amstel 51",
+    "latitude": "52.3681145",
+    "longitude": "4.9025139,15",
+    "city": "Amsterdam",
+    "postalCode": "1018EJ",
+    "email": "info@museumvandegeest.nl",
+    "phone": "+31235410681"
+}]
 
 function initMap() {
     // Create the map.
@@ -16,5 +34,19 @@ function initMap() {
         zoom: 14,
         center: myMapCenter,
     });
+
+    for (let i = 0; i < venues.length; i++) {
+        const marker = new google.maps.Marker({
+            position: { lat: parseFloat(venues[i].latitude), lng: parseFloat(venues[i].longitude) },
+            map: map,
+            title: venues[i].name
+        });
+    };
+
+    // new google.maps.Marker({
+    //     position: { lat: parseFloat(venue.latitude), lng: parseFloat(venue.longitude) },
+    //     map,
+    //     title: venue.name
+    // });
 
 };
