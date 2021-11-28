43117445352a99da20c88fbdfc90453bf4d70767
Folkert Stijnman <folkertstijnman@gmail.com>
2021-11-28 16:17:23 +0100

    default

--- js/main.js
+++
@@ -1,9 +0,0 @@
-function openSide() {
-    document.getElementById('leftpanel').style.width = "40%";
-    document.getElementById('rightpanel').style.width = "60%";
-}
-
-function closeSide() {
-    document.getElementById('leftpanel').style.width = "0%";
-    document.getElementById('rightpanel').style.width = "100%";
-}
